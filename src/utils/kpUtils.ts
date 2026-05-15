24ae687f5afba1a2a6e436cde770caaf7b59da77
bbdataviz <74002681+bbdataviz@users.noreply.github.com>
2026-05-15 17:04:04 +0200

    Refine geomagnetic storm classification

--- src/utils/kpUtils.ts
+++ src/utils/kpUtils.ts
@@ -14,34 +14,34 @@ export function getAuroraGradient(value : number) {
   }
 
   if (value >= 9) return auroraGradient(["#e60a69"]);
-  if (value >= 8) return auroraGradient(["#92068d"]);
-  if (value >= 7) return auroraGradient(["#ff21c0"]); 
-  if (value >= 6) return auroraGradient(["#f876ff"]);
-  if (value >= 5) return auroraGradient(["#53ed7c", "#f876ff"]);
-  if (value >= 4) return auroraGradient(["#3be067"]);
-  if (value >= 3) return auroraGradient(["#15bead"]);
+  if (value >= 7.67) return auroraGradient(["#92068d"]);
+  if (value >= 6.67) return auroraGradient(["#ff21c0"]); 
+  if (value >= 5.67) return auroraGradient(["#f876ff"]);
+  if (value >= 4.67) return auroraGradient(["#53ed7c", "#f876ff"]);
+  if (value >= 3.67) return auroraGradient(["#3be067"]);
+  if (value >= 2.67) return auroraGradient(["#15bead"]);
 
   return auroraGradient(["#2a7b9b"]);
 }
 
 export function getStormLevel(value : number) {
   if (value >= 9) return "G5";
-  if (value >= 8) return "G4";
-  if (value >= 7) return "G3";
-  if (value >= 6) return "G2";
-  if (value >= 5) return "G1";
+  if (value >= 7.67) return "G4";
+  if (value >= 6.67) return "G3";
+  if (value >= 5.67) return "G2";
+  if (value >= 4.67) return "G1";
 
   return "";
 }
 
 export function getStormDescription(value : number) {
   if (value >= 9) return "Extreme Geomagnetic Storm";
-  if (value >= 8) return "Severe Geomagnetic Storm";
-  if (value >= 7) return "Strong Geomagnetic Storm";
-  if (value >= 6) return "Moderate Geomagnetic Storm";
-  if (value >= 5) return "Minor Geomagnetic Storm";
-  if (value >= 4) return "Active Geomagnetic Activity";
-  if (value >= 3) return "Moderate Geomagnetic Activity";
+  if (value >= 7.67) return "Severe Geomagnetic Storm";
+  if (value >= 6.67) return "Strong Geomagnetic Storm";
+  if (value >= 5.67) return "Moderate Geomagnetic Storm";
+  if (value >= 4.67) return "Minor Geomagnetic Storm";
+  if (value >= 3.57) return "Active Geomagnetic Activity";
+  if (value >= 2.67) return "Moderate Geomagnetic Activity";
 
   return "Quiet";
 }
